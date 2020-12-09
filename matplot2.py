5c188d7b6a2c160e459c0d942f4d9c0e49693081
Karsten Werner <seremaster@gmail.com>
2020-12-09 11:53:31 +0100

    fff

--- matplot2.py
+++ matplot2.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 plt.style.use('seaborn')
 
-trees = pd.read_csv('insurance.csv')
+trees = pd.read_csv('fff.csv')
+trees.shape
 trees.head(5)
 print(trees.head(5))
