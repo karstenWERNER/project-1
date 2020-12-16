b18d403fb2c0f6d99ea6db3f2e8ba21222a87ca3
Karsten Werner <seremaster@gmail.com>
2020-12-16 07:10:00 +0100

    week 4

--- wine.py
+++ wine.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+#import model_evaluation_utils as meu
+from sklearn.model_selection import train_test_split
+from collections import Counter
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
 from scipy import stats
 
@@ -30,91 +35,119 @@ categories=['low', 'medium', 'high'])
 
 wines = pd.concat([red_wine, white_wine])
 
-wines.head(2)
+wtp_features = wines.iloc[:,:-3]
+wtp_feature_names = wtp_features.columns
+wtp_class_labels = np.array(wines['wine_type'])
 
-f, ax = plt.subplots(1, 2, figsize=(10, 4))
-sns.set_theme(style="ticks", palette="pastel")
-f.suptitle('Wine Quality - Alcohol Content/ Sulphates', fontsize=14)
+wtp_train_X, wtp_test_X, wtp_train_y, wtp_test_y = train_test_split(wtp_features,
+wtp_class_labels, test_size=0.3, random_state=42)
 
-f.subplots_adjust(top=0.85, wspace=0.3)
+print(Counter(wtp_train_y), Counter(wtp_test_y))
+print('Features:', list(wtp_feature_names))
+#wines.head(2)
+# Define the scaler
+wtp_ss = StandardScaler().fit(wtp_train_X)
+# Scale the train set
+wtp_train_SX = wtp_ss.transform(wtp_train_X)
+# Scale the test set
+wtp_test_SX = wtp_ss.transform(wtp_test_X)
+#f, ax = plt.subplots(1, 2, figsize=(10, 4))
+#sns.set_theme(style="ticks", palette="pastel")
+#f.suptitle('Wine Quality - Alcohol Content/ Sulphates', fontsize=14)
 
-sns.boxplot(x="wine_type", y="alcohol", data=wines, ax=ax[0])
+#f.subplots_adjust(top=0.85, wspace=0.3)
 
-ax[0].set_xlabel("Wine Quality Class",size = 12,alpha=0.8)
-ax[0].set_ylabel("Wine Alcohol %",size = 12,alpha=0.8)
+#sns.boxplot(x="wine_type", y="alcohol", data=wines, ax=ax[0])
 
-sns.boxplot(x="wine_type", y="sulphates", data=wines, ax=ax[1])
+#ax[0].set_xlabel("Wine Quality Class",size = 12,alpha=0.8)
+#ax[0].set_ylabel("Wine Alcohol %",size = 12,alpha=0.8)
 
-ax[1].set_xlabel("Wine Quality Class",size = 12,alpha=0.8)
-ax[1].set_ylabel("Wine Alcohol %",size = 12,alpha=0.8)
+#sns.boxplot(x="wine_type", y="sulphates", data=wines, ax=ax[1])
+
+#ax[1].set_xlabel("Wine Quality Class",size = 12,alpha=0.8)
+#ax[1].set_ylabel("Wine Alcohol %",size = 12,alpha=0.8)
 
 #sns.set_theme(style="ticks", palette="pastel")
 
-f, ax = plt.subplots(figsize=(10, 4))
+#f, ax = plt.subplots(figsize=(10, 4))
 
 # Draw a nested boxplot to show bills by day and time
-sns.boxplot(x="quality_label", y="alcohol", palette=["m", "g"], hue = 'wine_type',
-            data=wines, ax = ax)
+#sns.boxplot(x="quality_label", y="alcohol", palette=["m", "g"], hue = 'wine_type', data=wines, ax = ax)
+#sns.boxplot(x="quality_label", y="sulphates", hue = 'wine_type', data=wines, ax = ax)
 
-sns.despine(offset=10, trim=True)
-ax.legend(loc='upper right')
+#sns.despine(offset=10, trim=True)
+#ax.legend(loc='upper left')
 
-wines.quality_label.value_counts().plot(kind = 'bar')
+#plt.pie([x*100 for x in d_cuisine.values()],labels=[x for x in
+#d_cuisine.keys()],autopct='%0.1f',explode=[0,0,0.1,0])
 
-sns.kdeplot(data=wines, x="alcohol", hue = "quality" , multiple="stack")
+#wines['quality_label'].value_counts().plot.pie()
+#label the plot
 
-sns.kdeplot(data=wines[wines['wine_type'] == 'red'], x="alcohol")
+#plt.title('Wine Quality %')
 
-sns.kdeplot(data=wines[wines['wine_type'] == 'white'], x="alcohol")
 
-wines.plot.scatter(x='sulphates', y='alcohol')
 
-wines.columns 
 
-sns.lmplot(x='residual sugar', y='alcohol', data = wines, hue = 'wine_type')
 
-F, p = stats.f_oneway(wines[wines['quality_label'] == 'low']['alcohol'],
-wines[wines['quality_label'] == 'medium']['alcohol'],
-wines[wines['quality_label'] == 'high']['alcohol'])
-print('ANOVA test for mean alcohol levels across wine samples with different quality ratings')
-print('F Statistic:', F, '\tp-value:', p)
+#wines.quality_label.value_counts().plot(kind = 'bar')
 
-red_wine.hist(bins=15, color='red', edgecolor='black', linewidth=1.0,
-xlabelsize=8, ylabelsize=8, grid=False)
+#sns.kdeplot(data=wines, x="alcohol", hue = "quality" , multiple="stack")
 
-plt.tight_layout(rect=(0, 0, 1.2, 1.2))
+#sns.kdeplot(data=wines[wines['wine_type'] == 'red'], x="alcohol")
 
-rt = plt.suptitle('Red Wine Univariate Plots', x=0.65, y=1.25, fontsize=15)
+#sns.kdeplot(data=wines[wines['wine_type'] == 'white'], x="alcohol")
 
-fig = plt.figure(figsize = (10,4))
-title = fig.suptitle("Residual Sugar Content in Wine", fontsize=14)
-fig.subplots_adjust(top=0.85, wspace=0.3)
-ax1 = fig.add_subplot(1,2, 1)
-ax1.set_title("Red Wine")
-ax1.set_xlabel("Residual Sugar")
-ax1.set_ylabel("Frequency")
-ax1.set_ylim([0, 2500])
-ax1.text(8, 1000, r'$\mu$='+str(round(red_wine['residual sugar'].mean(),2)),
-fontsize=12)
-r_freq, r_bins, r_patches = ax1.hist(red_wine['residual sugar'], color='red', bins=15,
-edgecolor='black', linewidth=1)
-ax2 = fig.add_subplot(1,2, 2)
-ax2.set_title("White Wine")
-ax2.set_xlabel("Residual Sugar")
-ax2.set_ylabel("Frequency")
-ax2.set_ylim([0, 2500])
-ax2.text(30, 1000, r'$\mu$='+str(round(white_wine['residual sugar'].mean(),2)),
-fontsize=12)
-w_freq, w_bins, w_patches = ax2.hist(white_wine['residual sugar'], color='white', bins=15,
-edgecolor='black', linewidth=1)
+#wines.plot.scatter(x='sulphates', y='alcohol')
 
-rj = sns.jointplot(x='quality', y='sulphates', data=red_wine,
-kind='reg', ylim=(0, 2),
-color='red', space=0, size=4.5, ratio=4)
-rj.ax_joint.set_xticks(list(range(3,9)))
-fig = rj.fig
-fig.subplots_adjust(top=0.9)
-t = fig.suptitle('Red Wine Sulphates - Quality', fontsize=12)
+#wines.columns 
 
-plt.show()
+#sns.lmplot(x='residual sugar', y='alcohol', data = wines, hue = 'wine_type')
 
+#F, p = stats.f_oneway(wines[wines['quality_label'] == 'low']['alcohol'],
+#wines[wines['quality_label'] == 'medium']['alcohol'],
+#wines[wines['quality_label'] == 'high']['alcohol'])
+#print('ANOVA test for mean alcohol levels across wine samples with different quality ratings')
+#print('F Statistic:', F, '\tp-value:', p)
+
+#red_wine.hist(bins=15, color='red', edgecolor='black', linewidth=1.0,
+#xlabelsize=8, ylabelsize=8, grid=False)
+
+#plt.tight_layout(rect=(0, 0, 1.2, 1.2))
+
+#rt = plt.suptitle('Red Wine Univariate Plots', x=0.65, y=1.25, fontsize=15)
+
+#fig = plt.figure(figsize = (10,4))
+#title = fig.suptitle("Residual Sugar Content in Wine", fontsize=14)
+#fig.subplots_adjust(top=0.85, wspace=0.3)
+#ax1 = fig.add_subplot(1,2, 1)
+#ax1.set_title("Red Wine")
+#ax1.set_xlabel("Residual Sugar")
+#ax1.set_ylabel("Frequency")
+#ax1.set_ylim([0, 2500])
+#ax1.text(8, 1000, r'$\mu$='+str(round(red_wine['residual sugar'].mean(),2)),
+#fontsize=12)
+#r_freq, r_bins, r_patches = ax1.hist(red_wine['residual sugar'], color='red', bins=15,
+#edgecolor='black', linewidth=1)
+#ax2 = fig.add_subplot(1,2, 2)
+#ax2.set_title("White Wine")
+#ax2.set_xlabel("Residual Sugar")
+#ax2.set_ylabel("Frequency")
+#ax2.set_ylim([0, 2500])
+#ax2.text(30, 1000, r'$\mu$='+str(round(white_wine['residual sugar'].mean(),2)),
+#fontsize=12)
+#w_freq, w_bins, w_patches = ax2.hist(white_wine['residual sugar'], color='white', bins=15,
+#edgecolor='black', linewidth=1)
+
+#rj = sns.jointplot(x='quality', y='sulphates', data=red_wine,
+#kind='reg', ylim=(0, 2),
+#color='red', space=0, size=4.5, ratio=4)
+#rj.ax_joint.set_xticks(list(range(3,9)))
+#fig = rj.fig
+#fig.subplots_adjust(top=0.9)
+#t = fig.suptitle('Red Wine Sulphates - Quality', fontsize=12)
+
+#plt.show()
+
+
+#plt.savefig('C:\\Users\\HP\\Downloads\\Wein\\plots\\plotting_.png',dpi=300,bbox_inches='tight')
